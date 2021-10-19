af2c8dbe2a1b1951affecdd953219861803cb585
Gabriel <grgprofissional@gmail.com>
2021-10-19 19:07:12 -0300

    Fixing filename

---
+++ JS/01_Annalyn's_Infiltration.js
@@ -0,0 +1,8 @@
+/* https://exercism.org/tracks/javascript/exercises/annalyns-infiltration */
+
+
+let fastAttack,spy,signalPrisoner,freePrisioner;
+let knightIsAwake,archerIsAwake,prisonerIsAwake,dogIsPresent;
+
+
+
