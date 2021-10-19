f3bf3230c3df5b0e4e7620b9ce68d046c4cb4991
Gabriel <grgprofissional@gmail.com>
2021-10-19 20:06:56 -0300

    renamed the .js file

--- JS/01_Annalyn's_Infiltration.js
+++
@@ -1,8 +0,0 @@
-/* https://exercism.org/tracks/javascript/exercises/annalyns-infiltration */
-
-
-let fastAttack,spy,signalPrisoner,freePrisioner;
-let knightIsAwake,archerIsAwake,prisonerIsAwake,dogIsPresent;
-
-
-
